6b88dc4d9dfd0d7c930ae7b0c5b8d832682c62d9
Pranathi <pranathi374@gmail.com>
2026-04-05 20:56:11 +0530

    Fixed unused Navbar error

--- frontend/src/pages/Compare.js
+++ frontend/src/pages/Compare.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Sidebar from "../components/Sidebar";
-import Navbar from "../components/Navbar";
-
 function Compare() {
 
   const [repo1, setRepo1] = useState("");
